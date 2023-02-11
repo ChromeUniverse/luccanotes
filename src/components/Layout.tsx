4816d1cd1f14f60ff881b94723f217faa016ffb7
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2023-02-11 00:43:13 -0300

    Minor changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,16 +3,14 @@ import Navbar from "./Navbar";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="flex min-h-screen flex-col">
-        <Navbar username="lucca.dr" />
-        <main className=" flex-grow bg-gray-200 pt-4 pb-20">
-          <div className="mx-auto px-4 md:w-[90%] md:max-w-[1200px]">
-            {children}
-          </div>
-        </main>
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col">
+      <Navbar username="lucca.dr" />
+      <main className=" flex-grow bg-gray-200 pt-4 pb-20">
+        <div className="mx-auto px-4 md:w-[90%] md:max-w-[1200px]">
+          {children}
+        </div>
+      </main>
+    </div>
   );
 }
 
