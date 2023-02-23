f5a181e3c76878686eb1c364a755ea56005fa672
Lucca Rodrigues <33258966+ChromeUniverse@users.noreply.github.com>
2023-02-23 16:42:31 -0300

    Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,29 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
 import PageLayout from "../components/Layouts/Page";
+import { getServerAuthSession } from "../server/auth";
+
+export const getServerSideProps: GetServerSideProps<{
+  session: null;
+}> = async (context) => {
+  const session = await getServerAuthSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/notes",
+        permanent: false,
+      },
+    };
+  }
+
+  // Pass data to the page via props
+  return { props: { session: null } };
+};
 
 const Home: NextPage = () => {
-  return (
-    <PageLayout>
-      <h1>LuccaNotes</h1>
-    </PageLayout>
-  );
+  return <PageLayout container>welcome to LuccaNotes</PageLayout>;
 };
 
 export default Home;
